80022bf3a703c92fc2bdfd4eebe8b2712334fc68
yoongyum <72365815+yoongyum@users.noreply.github.com>
2022-04-24 20:35:06 +0900

    Time: 1385 ms (100.00%), Space: 18.8 MB (33.33%) - LeetHub

--- 2249-count-lattice-points-inside-a-circle/2249-count-lattice-points-inside-a-circle.py
+++ 2249-count-lattice-points-inside-a-circle/2249-count-lattice-points-inside-a-circle.py
@@ -1,12 +1,12 @@
 class Solution:
     def countLatticePoints(self, circles: List[List[int]]) -> int:
-        res = set([])
+        ans = set()
         for x,y,r in circles:
-            sx= x-r
-            sy = y-r
-            for sx in range(x+r+1):
-                for sy in range(y+r+1):
-                    if math.pow(r,2) >= math.pow(x-sx,2) + math.pow(y-sy,2):
-                        res.add((sx,sy))
-                     
-        return len(res)
+            for dx in range(r+1):
+                y_upper = int((r**2-dx**2)**0.5)
+                for dy in range(y_upper+1):
+                    ans.add((x+dx,y+dy))
+                    ans.add((x+dx,y-dy))
+                    ans.add((x-dx,y+dy))
+                    ans.add((x-dx,y-dy))
+        return len(ans)
