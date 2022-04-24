ad5e6451f3a08682383f6c3981a28f02b819b604
yoongyum <72365815+yoongyum@users.noreply.github.com>
2022-04-24 21:03:15 +0900

    Time: 182 ms (100.00%), Space: 20.9 MB (33.33%) - LeetHub

--- 2249-count-lattice-points-inside-a-circle/2249-count-lattice-points-inside-a-circle.py
+++ 2249-count-lattice-points-inside-a-circle/2249-count-lattice-points-inside-a-circle.py
@@ -1,12 +1,26 @@
 class Solution:
     def countLatticePoints(self, circles: List[List[int]]) -> int:
-        ans = set()
-        for x,y,r in circles:
-            for dx in range(r+1):
-                y_upper = int((r**2-dx**2)**0.5)
-                for dy in range(y_upper+1):
-                    ans.add((x+dx,y+dy))
-                    ans.add((x+dx,y-dy))
-                    ans.add((x-dx,y+dy))
-                    ans.add((x-dx,y-dy))
-        return len(ans)
+        intervals = [[] for _ in range(201)]
+        for x, y, r in circles:
+            for i in range(-r, r + 1):
+                d = math.floor(math.sqrt(r**2 - i**2))
+				# Add [start, 0] and [end, 1] to the list of intervals.
+                intervals[x + i].append([y - d, 0])
+                intervals[x + i].append([y + d, 1])
+        res = 0
+
+        for l in intervals:
+            if l:
+                l.sort()
+				# count of open intervals.
+                count = 0
+                for i, ind in l:
+                    if count == 0:
+                        s = i
+                    if ind == 0:
+                        count += 1
+                    else:
+                        count -= 1
+                        if count == 0:
+                            res += i - s + 1
+        return res
