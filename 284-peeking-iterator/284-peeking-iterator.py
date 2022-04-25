533a388a1377a74676dfc8a635243e7a9187f38c
yoongyum <72365815+yoongyum@users.noreply.github.com>
2022-04-25 19:51:27 +0900

    Time: 63 ms (10.87%), Space: 14.2 MB (29.71%) - LeetHub

---
+++ 284-peeking-iterator/284-peeking-iterator.py
@@ -0,0 +1,16 @@
+class PeekingIterator:
+  def __init__(self, iterator: Iterator):
+    self.iterator = iterator
+    self.buffer = self.iterator.next() if self.iterator.hasNext() else None
+
+  def peek(self) -> int:
+
+    return self.buffer
+
+  def next(self) -> int:
+    next = self.buffer
+    self.buffer = self.iterator.next() if self.iterator.hasNext() else None
+    return next
+
+  def hasNext(self) -> bool:
+    return self.buffer is not None
