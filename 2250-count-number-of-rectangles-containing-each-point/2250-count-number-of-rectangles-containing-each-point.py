1dc9cd207a9d83090ee89e56dd4615596fda39c3
Pikka <72365815+yoongyum@users.noreply.github.com>
2022-04-26 02:09:17 +0900

    Time: 2869 ms (80.00%), Space: 35.7 MB (80.00%) - LeetHub

---
+++ 2250-count-number-of-rectangles-containing-each-point/2250-count-number-of-rectangles-containing-each-point.py
@@ -0,0 +1,27 @@
+class Solution:
+    def countRectangles(self, rectangles: List[List[int]], points: List[List[int]]) -> List[int]:
+        from bisect import bisect
+
+        rectangles = sorted(rectangles, key=lambda x:x[0])
+
+        rectangle_map = defaultdict(list)
+
+        for l, h in rectangles:
+            rectangle_map[h].append(l)
+
+        def contains(x, y):
+            count = 0
+
+            for height, widths in rectangle_map.items():
+                if height >= y:
+                    count += len(widths) - bisect(widths, x - 1)
+
+            return count
+
+        res = []
+
+        for point in points:
+            x, y = point
+            res.append(contains(x, y))
+
+        return res
