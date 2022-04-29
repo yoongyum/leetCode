d1d5ad2d660cd92727e0cb57da4ca1808e382134
yoongyum <72365815+yoongyum@users.noreply.github.com>
2022-04-29 21:15:05 +0900

    Time: 181 ms (86.30%), Space: 14.4 MB (76.07%) - LeetHub

---
+++ 785-is-graph-bipartite/785-is-graph-bipartite.py
@@ -0,0 +1,23 @@
+from collections import deque
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        check = [-1]*len(graph)
+        
+        print(hash)
+        
+        for i in range(len(graph)):
+            Q = deque([])
+            if check[i] == -1:
+                Q.append(i)
+                check[i] = 0
+            while Q:
+                n1 = Q.popleft()
+                for n2 in graph[n1]:
+                    if check[n2] == -1:
+                        check[n2] = (check[n1] + 1)%2
+                        Q.append(n2)
+                    else:
+                        if check[n1] == check[n2]:
+                            return False
+        
+        return True
