8ef3fceb6bd54ffab7e887151cf1126a43dd2dd1
yoongyum <72365815+yoongyum@users.noreply.github.com>
2022-05-01 16:52:11 +0900

    Time: 42 ms (71.43%), Space: 13.8 MB (85.71%) - LeetHub

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.py
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.py
@@ -3,6 +3,6 @@ class Solution(object):
         answer= '0'
         for i in range(len(number)):
             if(number[i]==digit):
-                if(answer < (number[:i]+number[i+1:])):
+                if(answer < number[:i]+number[i+1:]):
                     answer= number[:i]+number[i+1:]
         return answer
