bb0ca695fdbabe958f3b0ea9c5b086d45ada8aca
yoongyum <72365815+yoongyum@users.noreply.github.com>
2022-05-05 18:37:46 +0900

    Time: 2588 ms (91.08%), Space: 77.9 MB (7.55%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.py
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.py
@@ -1,35 +1,40 @@
 class Solution:
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
-        tile = [[0]*n for i in range(m)]
-        
-        # 0은 공백
-        # 1은 가드
-        # 2는 벽
-        # 3은 가드가 왔던 곳
-        for x, y in guards:
-            tile[x][y] = 1
-        
-        for x, y in walls:
-            tile[x][y] = 2
+        H, W = m, n
+        M = [[0] * W for _ in range(H)]
+        ans = (H * W) - len(guards) - len(walls)
+
+        # free = 0
+        # wall = 1
+        # guard = 2
+        # guarded = 3
+
+        for r, c in walls: M[r][c] = 1
+        for r, c in guards: M[r][c] = 2
             
-        dx = [1,-1,0,0]
-        dy = [0,0,1,-1]
-        
-        
-        for x, y in guards:
-            for d in range(4):
-                i = x + dx[d]
-                j = y + dy[d]
-                while i >= 0 and i < m and j >= 0 and j < n:
-                    if tile[i][j] in (0,3):
-                        tile[i][j] = 3
-                        i += dx[d]
-                        j += dy[d]
-                    else : break
+        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
             
+        def dfs(y, x, d):
+            nonlocal ans
+            if 0 <= y < H and 0 <= x < W:
+                cell = M[y][x]
+                if cell == 2 or cell == 1:
+                    return
                 
+                if cell == 0:
+                    ans -= 1
+                    M[y][x] = 3
+                
+                dy, dx = y + d[0], x + d[1]
+                dfs(dy, dx, d)        
             
-        return sum(t.count(0) for t in tile)
+        for guard in guards:
+            r, c = guard
+            for d in dirs:
+                dy, dx = r + d[0], c + d[1]
+                dfs(dy, dx, d)    
+            
+        return ans
         
         
         
