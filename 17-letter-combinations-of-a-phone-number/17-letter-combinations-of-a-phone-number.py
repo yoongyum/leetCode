7eb23460d99d1dcc8ea5aa354e29e8f9c3408bc4
yoongyum <72365815+yoongyum@users.noreply.github.com>
2022-05-09 19:55:54 +0900

    Time: 51 ms (27.39%), Space: 13.8 MB (79.68%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -8,10 +8,10 @@ class Solution:
         def DFS(i, curS: str):
             if len(curS) == length:
                 ans.append(curS)
-            for index in range(i,length):
-                for c in tele[int(digits[index])]:
+            for idx in range(i,length):
+                for c in tele[int(digits[idx])]:
                     curS += c
-                    DFS(index+1,curS)
+                    DFS(idx+1,curS)
                     curS = curS[:-1]
                 
         DFS(0,"")
