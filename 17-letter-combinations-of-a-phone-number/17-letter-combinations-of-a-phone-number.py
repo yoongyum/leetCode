b9ef6ed3b17dc7e7fc95226389d3946d1be704c4
yoongyum <72365815+yoongyum@users.noreply.github.com>
2022-05-09 20:56:00 +0900

    Time: 36 ms (71.16%), Space: 14 MB (31.84%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,9 +1,9 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        if not digits : return [] 
         tel = ['###', '###','abc', 'def', 'ghi', 'jkl','mno', 'pqrs', 'tuv','wxyz']
         
         length = len(digits)
-        if length == 0 : return []
         ans = []
         def DFS(i, curS: str):
             if len(curS) == length:
