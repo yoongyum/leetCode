13265082a8ba261ea36f25f8b56f82aa39b319ae
yoongyum <72365815+yoongyum@users.noreply.github.com>
2022-07-29 00:11:31 +0900

    Time: 105 ms (15.39%), Space: 15.4 MB (11.53%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s=list(s)
+        t=list(t)
+        s.sort()
+        t.sort()
+        print(s,t)
+        return s == t
+        
