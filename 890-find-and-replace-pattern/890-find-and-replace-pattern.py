df0b833dcf1d248964f86220be2531edf7ee485e
yoongyum <72365815+yoongyum@users.noreply.github.com>
2022-07-29 18:57:14 +0900

    Time: 54 ms (44.46%), Space: 13.8 MB (76.64%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
+        result = []
+        for word in words:
+            if len(set(pattern)) == len(set(word)):
+                tempDict = {}
+                Flag = False
+                for i in range(len(pattern)):
+                    if pattern[i] not in tempDict:
+                        Flag= True
+                        tempDict[pattern[i]] = word[i]
+                    elif pattern[i] in tempDict and tempDict[pattern[i]] != word[i]:
+                        Flag = False
+                        break
+                if Flag== True:
+                    result.append(word)
+        return result
+        
