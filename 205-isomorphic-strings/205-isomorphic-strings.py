caa78facb033278acbfd32044a8d80e1726057e6
yoongyum <72365815+yoongyum@users.noreply.github.com>
2022-09-17 11:15:20 +0900

    Time: 96 ms (16.45%), Space: 14.9 MB (7.24%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.py
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -1,18 +1,9 @@
-from collections import defaultdict
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        shash = defaultdict(str)
-        thash = defaultdict(str)
-        for i in range(len(s)):  # 5*10_000
-            if s[i] in shash:
-                if shash[s[i]] != t[i]:
-                    return False
-            elif t[i] in thash:
-                if thash[t[i]] != s[i]:
-                    return False
-            else:
-                shash[s[i]] = t[i]
-                thash[t[i]] = s[i]
-    
-        return True
+        s1=[]
+        s2=[]
+        for i in range(len(s)):   #record the index of the 2 strings and then compare
+            s1.append(s.index(s[i]))
+            s2.append(t.index(t[i]))
+        return s1==s2
             
