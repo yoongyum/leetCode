9ac53e923c647f9c3e447ee21850ad7567762109
yoongyum <72365815+yoongyum@users.noreply.github.com>
2022-10-16 17:47:58 +0900

    Time: 99 ms (33.15%), Space: 13.8 MB (99.09%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -2,6 +2,8 @@ class Solution:
     def countAndSay(self, n: int) -> str:
         s = '1'
         for _ in range(n - 1):
-            s = ''.join(str(len(list(group))) + digit 
-                        for digit, group in itertools.groupby(s))
+            tmp = ''
+            for digit, group in itertools.groupby(s):
+                tmp += str(len(list(group))) + digit
+            s= tmp
         return s
